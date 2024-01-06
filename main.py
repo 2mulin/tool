53ee5ad393fa6adb994c7abf0856cd2ce17cb87a
2mu <1730911462@qq.com>
2024-01-06 23:19:29 +0800

    :recycle: refactor: 重构生成手机号的代码, 更短, 更简单

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 # -*- coding: utf-8 -*-
+"""
+@date   2024/1/6
+@author mulin
+"""
+
+import os
 
 import password_dict
 
 
 def main():
-    password_dict.generate_phone_num("./phone_num.txt")
+    """
+    主要工作流程
+    """
+    work_dir = os.getcwd()
+    # 工作产生的一些数据文件
+    data_path = f"{work_dir}/data"
+    if os.access(data_path, os.F_OK):
+        print(f"{data_path} already exists, prevent overwriting, program exit!")
+        return 1
+
+    try:
+        os.mkdir(data_path, 0o755)
+    except FileNotFoundError as e:
+        print(f"Failed to create data directory! reason: {e}")
+
+    password_dict_file = f"{data_path}/phone_num.txt"
+    if not password_dict.generate_phone_num(password_dict_file):
+        print("Failed to generate password file! ")
+        return 1
+    
+    
+    return 0
+
 
 if __name__ == '__main__':
-    main()
+    if main() == 1:
+        exit(1)
--- password_dict/phone_num.py
+++ password_dict/phone_num.py
@@ -1,82 +1,43 @@
 # -*- coding: utf-8 -*-
+"""
+生成电话号码相关的字典
+"""
 
-PHONE_NUM_LEN = 11
-# 移动电话前三位, 各自表示三大运营商; (物联网卡, 虚拟运行商, 卫星电话卡忽略...)
+# 移动电话前三位, 各自表示三大运营商; (当前忽略物联网卡, 虚拟运行商, 卫星电话卡)
 SERVICE_PROVIDER_NUM = [
     # 中国移动
-    [1,3,5],
-    [1,3,6],
-    [1,3,7],
-    [1,3,8],
-    [1,3,9],
-    [1,4,7],
-    [1,4,8],
-    [1,5,0],
-    [1,5,1],
-    [1,5,2],
-    [1,5,7],
-    [1,5,8],
-    [1,5,9],
-    [1,7,8],
-    [1,8,2],
-    [1,8,3],
-    [1,8,4],
-    [1,8,7],
-    [1,8,8],
-    [1,9,5],
-    [1,9,7],
-    [1,9,8],
+    135, 136, 137, 138, 139, 147, 148, 150, 151, 152,
+    157, 158, 159, 178, 182, 183, 184, 187, 188, 195,
+    197, 198,
     # 中国联通
-    [1,3,0],
-    [1,3,1],
-    [1,3,2],
-    [1,4,5],
-    [1,4,6],
-    [1,5,5],
-    [1,5,6],
-    [1,7,5],
-    [1,7,6],
-    [1,8,5],
-    [1,8,6],
-    [1,9,6],
+    130, 131, 132, 145, 155, 156, 175, 176, 185, 186,
+    196,
     # 中国电信
-    [1,3,3],
-    [1,4,9],
-    [1,5,3],
-    [1,7,3],
-    [1,7,7],
-    [1,8,0],
-    [1,8,1],
-    [1,8,9],
-    [1,9,0],
-    [1,9,1],
-    [1,9,3],
-    [1,9,9],
+    133, 149, 153, 173, 177, 180, 181, 189, 190, 191,
+    193, 199,
     # 中国广电
-    [1,9,2],
+    192,
 ]
-
-def _set_phone_num(phone_num_list: list[int], idx: int, file):
-    if idx == PHONE_NUM_LEN:
-        phone_num: str = ''.join([str(num) for num in phone_num_list])
-        file.write(phone_num + '\n')
-        return True
-
-    for num in range(0, 10):
-        phone_num_list[idx] = num
-        _set_phone_num(phone_num_list, idx+1, file)
 
 
 def generate_phone_num(filename: str) -> bool:
     """
-    @brief 生成所有可能的移动电话号码, 10的10次方, 有100亿行数据, 文件非常大....
-    所以缩减一些可能性; 号码的1~3位是运营商编号, 4~7位表示归属地;
-    @param filename 写入到指定的文件中
-    @return bool 成功写入指定文件则返回True
+    @brief 生成所有可能的11位电话号码, 如果不按规则, 有10的10次方种可能, 即100亿行数据, 文件非常大....
+    所以缩减一些可能性; 号码的1~3位是运营商编号, 4~7位表示归属地, 8~11位没有规则
+    @param  filename    写入到指定文件
+    @return bool        成功写入指定文件则返回True
     """
-    for provider in SERVICE_PROVIDER_NUM:
-        phone_num_list = provider + [0, 0, 0, 0, 0, 0, 0, 0]
-        with open(filename, 'w') as file:
-            _set_phone_num(phone_num_list, 3, file)
-    return True
-
+    is_success = True
+    for provider_num in SERVICE_PROVIDER_NUM:
+        assert len(str(provider_num)) == 3
+        phone_num = provider_num * 100000000
+        try:
+            with open(filename, 'w', encoding='utf-8') as file:
+                for _ in range(0, 100000000):
+                    phone_num += 1
+                    buf = f"{phone_num}\n"
+                    file.write(buf)
+        except Exception as e:
+            print(e)
+            is_success = False
+    return is_success
